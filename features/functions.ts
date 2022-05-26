58e601b396e6528a9b760e0bcbe806bf583b0df4
Jack Lambert <jacklmbrt07@gmail.com>
2022-05-25 21:01:03 -0700

    function annotation

---
+++ features/functions.ts
@@ -0,0 +1,15 @@
+const add = (a: number, b: number): number => {
+  return a + b;
+};
+
+const subtract = (a: number, b: number): number => {
+  return a - b;
+};
+
+function divide(a: number, b: number): number {
+  return a / b;
+}
+
+function multiply(a: number, b: number): number {
+  return a * b;
+}
