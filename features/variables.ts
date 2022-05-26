4f686c74a73ce92d37c4c242b3ac9b6207bb3e88
Jack Lambert <jacklmbrt07@gmail.com>
2022-05-25 20:31:43 -0700

    type annotations

--- features/variables.ts
+++ features/variables.ts
@@ -32,8 +32,24 @@ const logNumber: (i: number) => void = (i: number) => {
   console.log(i);
 };
 
-
 const json = '{"x": 10, "y": 20}';
 const coordinates = JSON.parse(json);
 console.log(coordinates);
- 
+
+let words = ["red", "green", "blue"];
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === "green") {
+    foundWord = true;
+  }
+}
+
+let numbers = [-10, -1, 12];
+let numberAboveZero: boolean | number = false;
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) {
+    numberAboveZero = numbers[i];
+  }
+}
